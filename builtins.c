ea3900aba478ebff1e1df270e62c9b5c7afdf591
GeorgII <georgii.galushko@gmail.com>
2022-09-17 14:18:15 +0300

    refactored builting handling

---
+++ builtins.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "builtins.h"
+#include "util.h"
+
+#define BUILTIN_DECLARE(NAME) static int NAME(int argc, char **argv)
+
+BUILTIN_DECLARE(cd)
+{
+	if (argc == 1)
+		return chdir(getenv("HOME"));
+	else
+		return chdir(argv[1]);
+}
+
+BUILTIN_DECLARE(bfork)
+{
+	int pid = fork();
+	if (pid == -1) {
+		perror("error: fork");
+		return -1;
+	}
+	if (pid == 0) {
+		return 0;
+	}
+	printf("%d", pid);
+	return 0;
+}
+
+struct table_entry {
+	char *name;
+	builtin fn;
+};
+
+static struct table_entry builtin_table[] = {
+	{ "cd",   cd },
+	{ "fork", bfork }
+};
+
+#define TABLESIZE (sizeof(builtin_table) / sizeof(*builtin_table))
+
+builtin
+find_builtin(const char *name)
+{
+	unsigned i;
+	for (i = 0; i < TABLESIZE; i++) {
+		if (str_equal(builtin_table[i].name, name)) {
+			return builtin_table[i].fn;
+		}
+	}
+	return NULL;
+}
